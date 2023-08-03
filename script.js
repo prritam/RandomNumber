a652b07af75b1b20307a9712c17479227f57a502
PRITAM MANE <manepritam81@gmail.com>
2023-08-03 18:52:22 +0530

    loop

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const prompt = require("prompt-sync")();
 const target=Math.round(Math.random()*100);
 console.log(target);
+while(true){
 
 const predict = prompt("Predict the random number");
 
 if(predict>target){
     console.log("Try lower value");
 }
-if(predict<target){
+ else if(predict<target){
     console.log("Try Higher value");
 }
 else{
     console.log("your guess is correct");
-}
+    break;
+}}
